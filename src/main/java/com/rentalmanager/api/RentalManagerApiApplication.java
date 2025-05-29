f26b73a78b69f5372d86a865e8ec25c728f3d38d
mayanklad <mayanklad12@gmail.com>
2025-05-29 18:35:44 -0400

    Initialize spring boot project using spring initializr

---
+++ src/main/java/com/rentalmanager/api/RentalManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.rentalmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentalmanager/api/RentalManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

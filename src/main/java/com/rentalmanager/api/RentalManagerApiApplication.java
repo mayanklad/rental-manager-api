69607bb6b10bb21fd7b73dbcecfe98b3b972e507
mayanklad <mayanklad12@gmail.com>
2025-07-04 01:13:28 -0400

    Enable CORS globally

--- src/main/java/com/rentalmanager/api/RentalManagerApiApplication.java
+++ src/main/java/com/rentalmanager/api/RentalManagerApiApplication.java
@@ -1,13 +1,35 @@
 package com.rentalmanager.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RentalManagerApiApplication {
 
+	@Value("${frontend.url}")
+	private String FRONTEND_URL;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RentalManagerApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(FRONTEND_URL)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+
+		};
 	}
 
 }
